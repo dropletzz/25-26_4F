277427b7968ac05897b70fea1dd720fd13d8d78e
skid <skid@kinkpad.local>
2025-09-25 09:38:42 +0200

    catalogo libri con inserimento e rimozione

--- java/06_CatalogoLibri/CatalogoLibri.java
+++ java/06_CatalogoLibri/CatalogoLibri.java
@@ -1,24 +1,77 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class CatalogoLibri {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // creo una lista di libri inizialmente vuota
         ArrayList<Libro> catalogo = new ArrayList<>();
+        // aggiungo due libri alla lista
+        catalogo.add(new Libro("10 piccoli di indiani", "Agatha Christie"));
+        catalogo.add(new Libro("Ubik", "Philip Dick"));
 
-        Libro l1 = new Libro("10 piccoli di indiani", "Agatha Christie");
-        // aggiungo il libro in fondo alla lista
-        catalogo.add(l1);
+        BufferedReader tastiera = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
 
-        Libro l2 = new Libro("Ubik", "Philip Dick");
-        catalogo.add(l2);
+        int scelta = 0;
+        do {
+            System.out.println("1 => Inserisci un libro");
+            System.out.println("2 => Elimina un libro");
+            System.out.println("3 => Elimina un libro per nome");
+            System.out.println("4 => Stampa i libri in catalogo");
+            System.out.println("0 => Esci");
+            scelta = Integer.parseInt(tastiera.readLine());
 
-        for (int i = 0; i < catalogo.size(); i++) {
-            // estraggo dalla lista il libro alla posizione i
-            // (equivalente a catalogo[i] negli array statici)
-            Libro lib = catalogo.get(i);
-            System.out.println(lib.toString());
-        }
+            switch (scelta) {
+                case 1: // nuovo libro
+                System.out.println("Inserisci titolo del libro");
+                String titolo = tastiera.readLine();
+                System.out.println("Inserisci autore del libro");
+                String autore = tastiera.readLine();
+
+                Libro nuovoLibro = new Libro(titolo, autore);
+                catalogo.add(nuovoLibro);
+                break;
+
+                case 2: // elimina libro a scelta
+                for (int i = 0; i < catalogo.size(); i++) {
+                    Libro lib = catalogo.get(i);
+                    System.out.println(i + ") " + lib.toString());
+                }
+                System.out.println("Inserisci l'indice del libro da rimuovere");
+                int indice = Integer.parseInt(tastiera.readLine());
+                catalogo.remove(indice);
+                break;
+
+                case 3: // elimina libro per titolo
+                System.out.println("Inserisci il titolo del libro da rimuovere");
+                String titoloInserito = tastiera.readLine();
+                int indiceLibroCercato = -1;
+                for (int i = 0; i < catalogo.size(); i++) {
+                    Libro lib = catalogo.get(i);
+                    if (titoloInserito.equals(lib.getTitolo())) {
+                        indiceLibroCercato = i;
+                    }
+                }
+                if (indiceLibroCercato >= 0) {
+                    catalogo.remove(indiceLibroCercato);
+                    System.out.println("Il libro e' stato rimosso");
+                }
+                else {
+                    System.out.println("Nessun libro trovato con il titolo inserito");
+                }
+                break;
+
+                case 4: // stampa elenco
+                for (int i = 0; i < catalogo.size(); i++) {
+                    Libro lib = catalogo.get(i);
+                    System.out.println(i + ") " + lib.toString());
+                }
+                break;
+            }
+        } while (scelta != 0);
+
     }
 }
