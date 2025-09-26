7436360fbd5387b79c66907a7749acc33abed4b3
skid <skid@kinkpad.local>
2025-09-26 15:24:39 +0200

    aggiornato CatalogoLibri, uso classe Leggi, genere con enum

--- java/06_CatalogoLibri/CatalogoLibri.java
+++ java/06_CatalogoLibri/CatalogoLibri.java
@@ -4,50 +4,50 @@ import java.util.ArrayList;
 public class CatalogoLibri {
 
     public static void main(String[] args) throws IOException {
-
         // creo una lista di libri inizialmente vuota
         ArrayList<Libro> catalogo = new ArrayList<>();
-        // aggiungo due libri alla lista
-        catalogo.add(new Libro("10 piccoli di indiani", "Agatha Christie"));
-        catalogo.add(new Libro("Ubik", "Philip Dick"));
 
-        BufferedReader tastiera = new BufferedReader(
-            new InputStreamReader(System.in)
-        );
+        // aggiungo due libri alla lista
+        catalogo.add(new Libro(
+            "10 piccoli di indiani",
+            "Agatha Christie",
+            Libro.Genere.GIALLO
+        ));
+        catalogo.add(new Libro(
+            "Ubik",
+            "Philip Dick",
+            Libro.Genere.FANTASCIENZA
+        ));
 
         int scelta = 0;
         do {
+            System.out.println("\n\n******CATALOGO LIBRI******");
             System.out.println("1 => Inserisci un libro");
             System.out.println("2 => Elimina un libro");
             System.out.println("3 => Elimina un libro per nome");
             System.out.println("4 => Stampa i libri in catalogo");
             System.out.println("0 => Esci");
-            scelta = Integer.parseInt(tastiera.readLine());
+
+            scelta = Leggi.interoTra("Scegli una opzione: ", 0, 4);
 
             switch (scelta) {
                 case 1: // nuovo libro
-                System.out.println("Inserisci titolo del libro");
-                String titolo = tastiera.readLine();
-                System.out.println("Inserisci autore del libro");
-                String autore = tastiera.readLine();
-
-                Libro nuovoLibro = new Libro(titolo, autore);
+                Libro nuovoLibro = leggiLibro();
                 catalogo.add(nuovoLibro);
                 break;
 
                 case 2: // elimina libro a scelta
-                for (int i = 0; i < catalogo.size(); i++) {
-                    Libro lib = catalogo.get(i);
-                    System.out.println(i + ") " + lib.toString());
+                if (catalogo.isEmpty()) {
+                    System.out.println("Nessun elemento da eliminare");
+                    break;
                 }
-                System.out.println("Inserisci l'indice del libro da rimuovere");
-                int indice = Integer.parseInt(tastiera.readLine());
-                catalogo.remove(indice);
+                stampaElenco(catalogo);
+                int indice = Leggi.interoTra("Inserisci l'indice del libro da rimuovere: ", 1, catalogo.size());
+                catalogo.remove(indice-1);
                 break;
 
                 case 3: // elimina libro per titolo
-                System.out.println("Inserisci il titolo del libro da rimuovere");
-                String titoloInserito = tastiera.readLine();
+                String titoloInserito = Leggi.stringa("Inserisci il titolo del libro da rimuovere: ");
                 int indiceLibroCercato = -1;
                 for (int i = 0; i < catalogo.size(); i++) {
                     Libro lib = catalogo.get(i);
@@ -65,13 +65,37 @@ public class CatalogoLibri {
                 break;
 
                 case 4: // stampa elenco
-                for (int i = 0; i < catalogo.size(); i++) {
-                    Libro lib = catalogo.get(i);
-                    System.out.println(i + ") " + lib.toString());
-                }
-                break;
+                stampaElenco(catalogo);
             }
         } while (scelta != 0);
 
+    }
+
+    public static Libro leggiLibro() {
+        String titolo = Leggi.stringa("Inserisci titolo del libro: ");
+        String autore = Leggi.stringa("Inserisci autore del libro: ");
+
+        System.out.println("1 = fantascienza");
+        System.out.println("2 = giallo");
+        System.out.println("3 = commedia");
+        System.out.println("4 = sconosciuto");
+        int sceltaGenere = Leggi.interoTra("Scegli un genere: ", 1, 4);
+
+        Libro.Genere genere;
+        switch (sceltaGenere) {
+            case 0: genere = Libro.Genere.FANTASCIENZA; break;
+            case 1: genere = Libro.Genere.GIALLO; break;
+            case 2: genere = Libro.Genere.COMMEDIA; break;
+            default: genere = Libro.Genere.SCONOSCIUTO; break;
+        }
+
+        return new Libro(titolo, autore, genere);
+    }
+
+    public static void stampaElenco(ArrayList<Libro> catalogo) {
+        for (int i = 0; i < catalogo.size(); i++) {
+            Libro lib = catalogo.get(i);
+            System.out.println((i+1) + ") " + lib.toString());
+        }
     }
 }
--- java/06_CatalogoLibri/Libro.java
+++ java/06_CatalogoLibri/Libro.java
@@ -1,16 +1,26 @@
 public class Libro {
+    public enum Genere {
+        FANTASCIENZA,
+        GIALLO,
+        COMMEDIA,
+        SCONOSCIUTO
+    }
+
     private String titolo;
     private String autore;
+    private Genere genere;
 
-    public Libro(String titolo, String autore) {
+    public Libro(String titolo, String autore, Genere genere) {
         this.titolo = titolo;
         this.autore = autore;
+        this.genere = genere;
     }
 
     public String getTitolo() {
         return titolo;
     }
 
+    @Override
     public String toString() {
         return  titolo + " di " + autore;
     }
